c5e49bd058c0c7cd676a948849e77ed79af92f52
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-05-10 07:32:16 +0000

    Python - import & modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,9 @@ import hidden_4
 if __name__ == '__main__':
     def_names = dir(hidden_4)
 
-    for i in range(len(def_names)):
-        if def_names[i][:2] != '__':
-            print(def_names[i])
+    def_names.sort()
+
+    for name in def_names:
+        if not name.startswith('__'):
+            print(name)
+
