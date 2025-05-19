16ce03473c30355c308727618e835453d704fdef
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-05-19 18:09:16 +0000

    Task2

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of the square (default is 0)
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
