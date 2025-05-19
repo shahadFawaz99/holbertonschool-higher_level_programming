ccf839b1bf9373a31f4e36ac49cc83d03e67143e
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-05-19 18:07:40 +0000

    Task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
