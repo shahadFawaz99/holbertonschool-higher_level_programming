ba02bc74d34aad78b4a94123c9614a52c41dca1e
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-06-01 20:21:24 +0000

    Task01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,8 +1,39 @@
-#!/usr/bin/env python3
-from task_01_duck_typing import Circle, Rectangle, shape_info
+#!/usr/bin/python3
+'''Modulo'''
+from abc import ABC, abstractmethod
+import math
 
-circle = Circle(radius=5)
-rectangle = Rectangle(width=4, height=7)
 
-shape_info(circle)
-shape_info(rectangle)
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = abs(radius)
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    '''clase rectangulo'''
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width * 2) + (self.height * 2)
+
+def shape_info(shape):
+    print(f"Area: {shape.area():.1f}")
+    print(f"Perimeter: {shape.perimeter():.1f}")
