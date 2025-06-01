d8ded0ad43aa8fcb62579afe84c17e416ee51a84
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-06-01 20:23:03 +0000

    Task01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''Modulo'''
+"""Module defining abstract shapes and concrete implementations using duck typing."""
+
+
 from abc import ABC, abstractmethod
 import math
 
@@ -8,9 +10,11 @@ class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
+
     @abstractmethod
     def perimeter(self):
         pass
+
 
 class Circle(Shape):
     def __init__(self, radius):
@@ -22,8 +26,10 @@ class Circle(Shape):
     def perimeter(self):
         return 2 * math.pi * self.radius
 
+
 class Rectangle(Shape):
-    '''clase rectangulo'''
+    """Class representing a rectangle."""
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -33,6 +39,7 @@ class Rectangle(Shape):
 
     def perimeter(self):
         return (self.width * 2) + (self.height * 2)
+
 
 def shape_info(shape):
     print(f"Area: {shape.area():.1f}")
