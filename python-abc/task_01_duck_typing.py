58ab2b30c5a3f6c73e4ba5bd0bb3b660f6937e57
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-06-01 20:17:23 +0000

    Task01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import math
 
+
 class Shape(ABC):
     @abstractmethod
     def area(self):
@@ -10,15 +11,17 @@ class Shape(ABC):
     def perimeter(self):
         pass
 
+
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
         return 2 * math.pi * self.radius
+
 
 class Rectangle(Shape):
     def __init__(self, width, height):
@@ -31,6 +34,7 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2 * (self.width + self.height)
 
+
 def shape_info(shape):
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
