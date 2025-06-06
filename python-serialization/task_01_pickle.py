93628af1f7c213d8b22b74691d2dbb2b1c6cc124
shahadFawaz99 <shahadfwaz2@gmai.com>
2025-06-06 01:45:00 +0000

    Task01

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-"""Module to pickle and unpickle a custom class."""
-
+"""erialize and deserialize custom Python objects using the pickle"""
 import pickle
 
 
 class CustomObject:
-    """Custom object with name, age, and student status."""
-
-    def __init__(self, name, age, is_student):
+    def __init__(self, name: str, age: int, is_student: bool):
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """Display the attributes of the object."""
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
+        print(f"Name: {self.name}\nAge: {self.age}\n\
+              Is Student: {self.is_student}".format())
 
     def serialize(self, filename):
-        """Serialize the object and save it to a file."""
-        try:
-            with open(filename, 'wb') as f:
-                pickle.dump(self, f)
-        except (OSError, pickle.PickleError):
-            pass  # You can optionally log or print the error
+        """serialize the object and save it to file"""
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
 
-    @classmethod
-    def deserialize(cls, filename):
-        """Deserialize the object from a file."""
+    def deserialize(filename):
+        """deserialize data from file and return"""
         try:
-            with open(filename, 'rb') as f:
-                obj = pickle.load(f)
-                if isinstance(obj, cls):
-                    return obj
-        except (OSError, pickle.PickleError):
-            pass
-        return None
+            with open(filename, "rb") as file:
+                self = pickle.load(file)
+        except Exception as e:
+            return None
+        return self
