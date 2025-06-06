54f604b8fbf12fa3591b02f80236fddf17c52de6
shahadFawaz99 <shahadfwaz2@gmai.com>
2025-06-06 01:14:39 +0000

    Task6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""
-Function that creates a 
-Python object from a JSON file.
-"""
+"""Module to load an object from a JSON file."""
+
 import json
 
 
 def load_from_json_file(filename):
-    """
-    Creates a Python object from a JSON file.
-
-    Args:
-        filename (str): The path to the JSON file.
-
-    Returns:
-        The Python object resulting from the 
-        deserialization of the JSON file content.
-    """
-    with open(filename, "r", encoding="utf-8") as f:
+    """Create an Object from a JSON file."""
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
