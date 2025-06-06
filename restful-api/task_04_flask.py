fe625a39de89971e05e46939181a63d02521d389
shahadFawaz99 <shahadfwaz2@gmai.com>
2025-06-06 02:20:30 +0000

    Task04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,11 +2,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# In-memory users dictionary
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"},
-}
+# In-memory users dictionary should start empty for tests to pass
+users = {}
 
 @app.route('/')
 def home():
