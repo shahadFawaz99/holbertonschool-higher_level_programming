0082bb2e4daab964add8830508084f24e9175504
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-06-29 17:41:17 +0000

    task10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument from the database."""
+"""
+Prints the State object with the name passed as argument from the db
+"""
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    # Create engine to connect to the MySQL database
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
 
-    # Start a session
-    session = Session(engine)
-
-    # Safely query the State object matching the provided name
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    # Print the result or Not found
-    if state:
-        print(state.id)
-    else:
-        print("Not found")
-
-    # Close the session
-    session.close()
+    print("Not found" if not state else state.id)
