25d857894c7018ed65cff726c5ebeb78eec49d90
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-06-29 18:14:59 +0000

    Task10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
--- Select the show title and genre_id from the tv_shows and tv_show_genres tables
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
--- Join the tv_show_genres table on show id
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
--- Order results by title then genre_id ascending
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Script to list all shows with their genre IDs using JOIN
+-- This script displays tv_shows.title - tv_show_genres.genre_id
+
+-- Select shows with their genre IDs using JOIN
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
