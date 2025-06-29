62fd507d3a8a3f7846d311d0198b9535762ac2ca
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-06-29 17:10:34 +0000

    task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
 """
-Lists all states from the database passed as argument
-Usage: ./0-select_states.py username password database_name
+Script that lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+Results sorted by states.id ascending
 """
 
 import sys
 import MySQLdb
 
-def main():
-    if len(sys.argv) != 4:
-        return
-
+if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=db_name)
-    cursor = db.cursor()
+    # Connect to MySQL server on localhost, port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=db_name, charset="utf8")
 
-    # Execute query to get all states sorted by id ascending
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all results and print them
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    main()
