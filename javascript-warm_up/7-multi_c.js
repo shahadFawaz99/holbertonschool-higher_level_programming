8cbb843cd953ac1ab44ce90f869919c92f99cb6e
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-07-16 17:18:40 +0000

    task7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-
-const x = parseInt(process.argv[2]);
-
-if (isNaN(x) || x <= 0) {
-  console.log('Missing number of occurrences');
-} else {
-  let i = 0;
-  while (i < x) {
+if (process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
-    i++;
   }
+} else {
+  console.log('Missing number of occurrences');
 }
