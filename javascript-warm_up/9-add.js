efc13ff466485aa4af32a7ec940544ce1acde656
shahadFawaz99 <shahadfwaz2@gmail.com>
2025-07-16 17:21:11 +0000

    task9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
-
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-
-console.log(add(a, b));
+console.log(add(process.argv[2], process.argv[3]));
